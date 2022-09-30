aa53203afccbf055e29fd5af8590c9306ca97843
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-09-30 05:35:26 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 int main()
-{
-		string initialisation
-		    char Mystr[] = "Programming is like building a multilingual puzzle;
-		        
-		            puts(Mystr)t
-		                
-		                    return 0;
-		                    }
+{	
+char Mystr[] ="\"Programming is like building a multilingual puzzle";
+puts(Mystr);		                
+return 0;
+ }
