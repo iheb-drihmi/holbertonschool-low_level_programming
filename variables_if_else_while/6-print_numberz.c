b675c0a7a9e2b3631533914a8d8e8e614867ec12
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-03 13:00:21 -0700

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,6 @@
-nclude <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -6,5 +8,13 @@ nclude <time.h>
  * Return: Always 0 (Success)
  */
 
-int main(void) for (d = '0'; d <= '9'; d++)
-		putchar(d);{
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
