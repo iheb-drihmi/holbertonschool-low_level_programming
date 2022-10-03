4a30856355276d604069250b1d166ca1926f54ca
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-03 06:58:56 -0700

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,15 +4,18 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void) {
-   char ch;
+int main (void) 
+{
+   	char ch;
 
-   for(ch = 'a' ; ch <= 'z' ; ch++) {
-      putchar(ch);
-   }
-    for(ch = 'A' ; ch <= 'Z' ; ch++) {
-      putchar(ch);
-   }
-   putchar('\n'); 
+   	for(ch = 'a' ; ch <= 'z' ; ch++) 
+	{
+     	 putchar(ch);
+ 	  }
+    	for(ch = 'A' ; ch <= 'Z' ; ch++) 
+	{
+      	putchar(ch);
+   	}
+   	putchar('\n'); 
 	 return(0);
 }
