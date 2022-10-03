5a4545a8039089bb7ae59f31f16a81a1e25e3458
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-03 02:12:06 -0700

    positive anything is better

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <studlib.h>
+#include <time.h>
+/**
+ * main -Print if n is positive or negative 
+ *
+ * Return:Always 0 (success)
+ */
+int main (void)
+{
+	int n;
+	
+	rand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+	{
+	printf("%d is positive\n",n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n",n);
+	}
+	else 
+	{
+	printf("%d is negative\n",n);
+	}		
+	return(0)
+}
