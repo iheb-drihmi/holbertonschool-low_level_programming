146e393bde2560df7a6bcdb988142c75d1627562
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-04 05:45:28 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - prints alphabet x10 times
+  *
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	char l;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (l = 'a'; l <= 'z'; c++)
+		{
+			_putchar(l);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
+
