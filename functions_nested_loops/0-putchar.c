d24b6ac35b85085d795340cc697f598d17c2f428
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-04 03:30:42 -0700

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,15 +7,8 @@
  */
 int main(void)
 {
-	_putchar('-');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar")
+	printf("\n")	
 	return(0);
 }
 
