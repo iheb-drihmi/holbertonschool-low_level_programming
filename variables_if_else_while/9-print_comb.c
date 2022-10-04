212d6121b02b64351c44dc4b1a27be790b10759c
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-04 02:34:32 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int n;
 
-	for (n ='0'; n <= '9' ; n++)
-{
+	for (n = '0' ; n <= '9' ; n++)
+	{
 	putchar ('n');
 	if( n == '9')
-	continue ;
-	putchar(',');
-	putchar(' ');	
-}
+	continue;
+	putchar (',');
+	putchar (' ');
+	}
 	putchar ('\n');
 	return(0);
-} 
+}
 
