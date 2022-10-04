f7bda5c7e57391e686cae607572dfca15a0d7efa
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-04 04:56:47 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - Print alphabet in lowercase
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	int l;
 	for (l = 'a' ; l <= 'z' ; l++)
