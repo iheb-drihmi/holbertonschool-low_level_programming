50e6a2d9d27420c2a206d7f3220a28cb35e7fa6c
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-04 01:29:04 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Print all possible combinations of single-digit numbers
+ * Return : always (0) succes
+ */
+int main (void)
+{
+	int n ;
+	for (n='0' ; n <='9' ;n ++)
+	{
+	putchar (n);
+	if ( n == '9' )
+	continue ;
+	putchar (',');
+	putchar (' ');	
+	}
+	putchar ('\n');
+	return(0);;
+} 
