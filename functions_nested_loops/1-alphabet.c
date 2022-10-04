63fcedc9004befe9a2bfeab823e254ab751b52d5
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-04 04:53:57 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int l;
+	for (l = 'a' ; l <= 'z' ; l++)
+	{ 
+     	_putchar(l) ;
+	}
+	_putchar('\n');
+	 return (0);
+}
+
