1181f7f29478eba4640ec6c0c042158889f7e816
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-05 01:06:37 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * islower - check for lowercase character
+ *
+ * Return: 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
