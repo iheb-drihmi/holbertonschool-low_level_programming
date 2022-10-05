d80998bd1d3a53c4113b76af3c015d70199c98f9
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-05 01:17:44 -0700

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * islower - check for lowercase character
- *
+ * Islower - check for lowercase character
+ * Description - check for lowercase character
  * Return: 1
  */
 int _islower(int c)
