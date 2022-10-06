8adf6ed6346e5a23a200c4ec2209279bfa03df6d
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-06 02:57:46 -0700

    10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,12 @@
 #include"main.h"
+
 /**
- * Return:The result.
- */
+  * add - Adds two results
+  * @num1: The first result
+  * @num2: The second  result
+  *
+  * Return: Integer value
+  */
 int add(int num1, int num2)
 {
 	return (num1 + num2);
