ffb1a4fd3995f4c0d6d78f8ddf6fa7b036544bee
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-07 11:05:38 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,11 +5,26 @@
  */
 void times_table(void)
 {
-int n, i;
-   printf("Enter an integer: ");
-   scanf("%d", &n);
-  for (i = 0; i <= 9; ++i) {
-     printf("%d * %d = %d \n", n, i, n * i);
-  }
-  
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+
+		for (b = 1; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			c = a * b;
+
+			if (c <= 9)
+				_putchar(' ');
+			else
+				_putchar((c / 10) + '0');
+
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
