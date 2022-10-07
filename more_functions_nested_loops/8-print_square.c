8dbd9d97ef393e8b7de334e827a0c20c2c6d219e
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-07 10:03:00 -0700

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*print_square - prints squares
+*@size: parameter
+*Return:returns nothing
+*/
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
