ea72e654f2a07f098d4e124ed8e34337f61789c3
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-07 10:22:33 -0700

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - multplies of 3 or 5
+ *
+ * Description: numbers 1 to 100 
+ * Return: Always(0)
+ *
+ * /
+int main(void)
+{
+int mul;
+while (mul++ < 100)
+
+if ((mul % 3 == 0) && (mul % 5 == 0))
+printf("FizzBuzz ");
+
+else if ((mul % 3) == 0)
+printf("Fizz ");
+
+else if ((mul % 5) == 0)
+{
+if (mul != 100)
+printf("Buzz ");
+
+else
+printf("Buzz");
+}
+
+else
+printf("%d ", mul);
+
+printf("\n");
+return (0);
+}
