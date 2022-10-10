f4f6ac605582f8dab9c6b7880c840ee09ea0c9ea
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-10 03:06:56 -0700

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * reset_to_98 - takes a pointer to an int as paremeter and updates the value 
+ * reset_to_98 -takes a pointer to an int as paremeter and updates the value 
  *
  * @n: pointer to be update
  */
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
