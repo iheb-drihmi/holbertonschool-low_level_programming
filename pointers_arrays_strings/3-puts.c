df0734af178f4004e4285d8d017da6524354dcb9
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-10 06:54:21 -0700

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts -print a stirng 
+ *@str: print
+ */
+void _puts(char *str)
+{
+	int ch = 0;
+
+	while (str[ch] != '\0')
+	{
+		_putchar(str[ch]);
+		ch++;
+	}
+	_putchar('\n');
+}
