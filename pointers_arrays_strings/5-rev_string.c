bb4da5b74f4f24e8501c7449245a76deb0486b27
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-11 04:07:18 -0700

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,14 @@ void rev_string(char *s)
 	int a, i, c;
 	char b;
 
-	for (a = 0; s[a]!= '\0'; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	;
 	i = 0;
 	c = a / 2;
 	while (c--)
 	{
-	b = s[a -i -1];
-	s[a -i - 1] = s[i];
+	b = s[a - i - 1];
+	s[a - i - 1] = s[i];
 	s[i] = b;
 	i++;
 	}
