e9451042b8c6f7ade8cf7b39e1896ba7876aff07
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-11 00:43:41 -0700

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,12 @@
  */
 void print_rev(char *s)
 {
-        int ch = 0;
+int ch = 0;
 
-        while (s[ch])
-               ch++;
-        while	(ch--)
-		_putchar(s[ch]);
+while (s[ch])
+ch++;
+while(ch--)
+_putchar(s[ch]);
 		
-        _putchar('\n');
+_putchar('\n');
 }
