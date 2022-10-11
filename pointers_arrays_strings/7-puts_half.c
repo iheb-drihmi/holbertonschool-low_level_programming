aa535e1830ff7d3bd7f53409048b501ba0130263
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-11 07:37:38 -0700

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,16 +6,24 @@
  *
  *Return: return
  */
-
 void puts_half(char *str)
-{
-int i;
 
-for (i = 0; str[i] != '\0'; i++)
 {
-if (i % 2 == 1)
+int i, half;
+
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+
+half = (i + 1) / 2;
+
+for (i = half; str[i]; i++)
+{
 _putchar (str[i]);
 }
 
 _putchar ('\n');
+
 }
