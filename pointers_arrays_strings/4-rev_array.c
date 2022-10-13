db0827977a1a3f236f2f6b14db03eae01f3c5235
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-13 12:15:18 -0700

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - a function that reverse array of integers
+ * @a: array
+ * @n: number 
+ * Return: return
+ */
+void reverse_array(int *a, int n)
+{
+	int b;
+	int tmp;
+
+	for (b = 0; i < n--; b++)
+	{
+		tmp = a[b];
+		a[b] = a[n];
+		a[n] = tmp;
+	}
+}
