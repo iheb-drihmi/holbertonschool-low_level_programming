16bd8bcacec82ef15735baf7939e9b3c29d81d05
iheb-drihmi <ihebdrihmi2002@gmail.com>
2022-10-18 03:58:42 -0700

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strlen_recursion: a function that return the length of a string.
- *@s: character
- * Description : retunr the length of a string
+ *Description: a function that return the length of a string.
+ * @s:  a string
  * Return: return
  */
 int _strlen_recursion(char *s)
