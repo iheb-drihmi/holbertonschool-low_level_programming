5cd8e9d7cf2e645069799dcc7e2d0dc604be4e89
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-10-20 09:04:10 +0100

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - a program that prints it's name 
-*@argc: number 
-*@argv: array 
+* main - a program that prints it's name
+*@argc: number
+*@argv: array
 *Return: return
 */
 int main(int __attribute__((__unused__)) argc, char *argv[])
