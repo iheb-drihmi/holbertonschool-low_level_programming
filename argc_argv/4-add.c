b2adb651abb8f22271e9cf64cf97ed9b8297453f
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-10-20 15:45:10 +0100

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - a program that adds positive numbers
+* @argc: count
+* @argv: vector
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b]; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[a]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
