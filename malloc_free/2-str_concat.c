3abc118bce355c5199e8978d77b46e67a2d14cc0
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-10-24 21:00:00 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,3 +1,10 @@
+#include "main.h"
+/**
+* str_concat - a function that concatenates two strings
+*@s1:The first string
+*@s2:The second string
+*Return: return
+*/
 char *str_concat(char *s1, char *s2)
 {
 	char *str_concat;
