1a9d6db5b117ddf0aff9aa5938f763bb713c9216
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-10-26 16:27:27 +0100

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  *malloc_checked - a function that allocates memory using malloc
- *@b:unsigned int 
+ *@b:unsigned  int 
  *Return:return
  */
 void *malloc_checked(unsigned int b)
