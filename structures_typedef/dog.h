b56300ee5ef45bfa2d241d8530ddd133c5855cd4
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-10-31 09:32:48 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct dog
+ * @name: The name
+ * @age: The age
+ * @owner: The owner
+ * Description: The dog
+ */
+
+ struct dog{
+     char *name;
+     float age;
+     char *owner;
+ };
+#endif DOG_H
