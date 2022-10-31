03b16603982854e96b473ee9635be385cebbdce4
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-10-31 10:08:42 +0100

    init_dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include<stdio.h>
+
+/**
+ * init_dog - initialiser dog
+ * @a: The animal
+ * @name: The name
+ * @age: The age
+ * @owner: The owner
+ */
+ void init_dog(struct dog *d, char *name, float age, char *owner)
+ {
+    if (a != NULL)
+    {
+        a->name = name;
+        a->age = age;
+        a->owner = owner;
+    }
+ }
