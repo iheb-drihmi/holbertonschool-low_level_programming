c0161d49877851ce3b60bdc52ab0d256bf534031
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-04 21:11:50 +0100

    new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,34 @@
+#include "dog.h"
+
+
+/**
+*_strlen - returns length of
+*@str: str
+*Return: returns
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (str)
+len++;
+
+return (len);
+}
+
+
+/**
+*_strcopy - copy string pointed by src
+*@dest:dest
+*@src: src
+*Return:return
+*/
+char *_strcopy(char *dest, char *src)
+{
+int index = 0;
+
+for (; src[index] ; index++)
+dest[index] = src[index];
+
+dest[index] = '\0';
+return (dest);
+}
