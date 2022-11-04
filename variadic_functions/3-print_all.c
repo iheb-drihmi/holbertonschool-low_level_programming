aff832490b7b9100a857b208933e834c964c4d60
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-04 20:44:54 +0100

    3-print_all.c

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,32 @@
+void print_all(const char * const format, ...)
+{
+	va_list countptr;
+	int x, arg_len;
+	char *separator = "";
+
+	prints_t fmt_list[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+	va_start(countptr, format);
+	x = 0;
+	while (format && (*(format + x)))
+	{
+		arg_len = 0;
+		while (arg_len < 4 && (*(format + x) != *(fmt_list[arg_len].notation)))
+		{
+			arg_len++;
+		}
+		if (arg_len < 4)
+		{
+			printf("%s", separator);
+			fmt_list[arg_len].print_dType(countptr);
+			separator = ", ";
+		}
+		x++;
+	}
+	printf("\n");
+	va_end(countptr);
+}
