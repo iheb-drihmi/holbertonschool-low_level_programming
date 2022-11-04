ea0ab4107ad6cf518a85ea4b3a0d386e23251add
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-04 19:16:20 +0100

    1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers.
+ * @separator:separator
+ * @n: number
+ *
+ * Return: no return.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list countptr;
+	unsigned int x;
+
+	va_start(countptr, n);
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(countptr, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(countptr);
+}
