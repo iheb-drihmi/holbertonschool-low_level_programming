df6e1d8eaec4f51e06959a178882844e7bf45b6d
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-15 11:38:09 +0100

    print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "list.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+*print_list - print list 
+*@h: the list
+*Return : return
+*/
+size_t print_list(const list_t *h)
+{
+    size_t nodes=0;
+    while (h)
+    {
+    if (str == NULL)
+    printf ("[0] (nil)\n");
+    else
+    printf ("[%d]%s\n,h->len,h->str");
+    nodes ++;
+    h = h->next;
+    }
+    return (nodes);
+}
+
