81f91e7a9fa241d038a13495ae6ffeef94c5a5a6
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-15 12:58:17 +0100

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include"lists.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+*list_len - a function that return the number of elements in a linked list
+*@h: the list
+*Return: Return nodes
+*/
+size_t list_len(const list_t *h)
+{
+    size_t nodes = 0;
+    while (h)
+    {
+        nodes++;
+        h = h->nesxt;
+    }
+    return (nodes);
+}
