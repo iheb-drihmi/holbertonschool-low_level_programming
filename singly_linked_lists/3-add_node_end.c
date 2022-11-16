482b22384d1000b24ad36985f0118a270266475d
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-16 11:27:24 +0100

    3-add_node_end.c

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * add_node_end - a function that adds a new node at the end
+ * @head: head
+ * @str: string
+ * Return: return
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new, *tmp;
+    size_t newchar;
+
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->str = strdup(str);
+
+    for (newchar = 0; str[newchar]; newchar++)
+        ;
+
+    new->len = newchar;
+    new->next = NULL;
+    tmp = *head;
+
+    if (tmp == NULL)
+    {
+        *head = new;
+    }
+    else
+    {
+        while (tmp->next != NULL)
+            tmp = tmp->next;
+        temp->next = new;
+    }
+
+    return (*head);
+}
+
+
+
+
+
+
+
