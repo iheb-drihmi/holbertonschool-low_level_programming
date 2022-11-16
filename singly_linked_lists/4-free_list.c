94de0c9f4146433c7c4cdb41a3f3a9021113e120
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-16 13:13:13 +0100

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: return.
+ */
+
+void free_list(list_t *head)
+{
+    list_t *f;
+
+    while ((f = head) != NULL)
+    {
+        head = head->next;
+        free(f->str);
+        free(f);
+    }
+}
