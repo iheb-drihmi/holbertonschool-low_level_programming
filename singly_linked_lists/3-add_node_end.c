663a06e626b7878282289e207eadd7b1be7f3647
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-16 11:33:10 +0100

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,34 +12,34 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new, *tmp;
-    size_t newchar;
+list_t *new, *tmp;
+size_t newchar;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
 
-    new->str = strdup(str);
+new->str = strdup(str);
 
-    for (newchar = 0; str[newchar]; newchar++)
-        ;
+for (newchar = 0; str[newchar]; newchar++)
+    ;
 
-    new->len = newchar;
-    new->next = NULL;
-    tmp = *head;
+new->len = newchar;
+new->next = NULL;
+tmp = *head;
 
-    if (tmp == NULL)
-    {
-        *head = new;
-    }
-    else
-    {
-        while (tmp->next != NULL)
-            tmp = tmp->next;
-        tmp->next = new;
-    }
+if (tmp == NULL)
+{
+*head = new;
+}
+else
+{
+while (tmp->next != NULL)
+tmp = tmp->next;
+tmp->next = new;
+}
 
-    return (*head);
+return (*head);
 }
 
 
