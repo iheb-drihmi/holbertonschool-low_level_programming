640c7c7932cb60eda9aa8254100111bd208e4304
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-17 20:36:00 +0100

    6-pop_listint.c

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,13 @@
  */
 int pop_listint(listint_t **head)
 {
-    listint_t *tmp;
-    int number;
-
-    if (!head || !*head)
-        return (0);
-
-    number = (*head)->n;
-    tmp = (*head)->next;
-    free(*head);
-    *head = tmp;
-
-    return (number);
+listint_t *tmp;
+int number;
+if (!head || !*head)
+return (0);
+number = (*head)->n;
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+return (number);
 }
