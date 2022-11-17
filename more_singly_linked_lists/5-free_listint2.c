6111e6615fcfa66cb2307a3173fe16d815899e23
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-17 18:21:41 +0100

    5-free_listint2.c

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,17 +9,14 @@
  */
 void free_listint2(listint_t **head)
 {
-    listint_t *tmp;
-
-    if (head == NULL)
-        return;
-
-    while (*head)
-    {
-        tmp = (*head)->next;
-        free(*head);
-        *head = tmp;
-    }
-
-    *head = NULL;
+listint_t *tmp;
+if (head == NULL)
+return;
+while (*head)
+{
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+}
+*head = NULL;
 }
