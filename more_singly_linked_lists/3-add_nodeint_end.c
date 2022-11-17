0b7bf84b804f78b6f8ad3fbe7755859522fffead
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-17 18:05:59 +0100

    3-add_nodeint_end.c

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,26 +11,20 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new;
-    listint_t *tmp = *head;
-
-    new = malloc(sizeof(listint_t));
-    if (!new)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
-
-    while (tmp->next)
-        tmp = tmp->next;
-
-    tmp->next = new;
-
-    return (new);
+listint_t *new;
+listint_t *tmp = *head;
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
+while (tmp->next)
+tmp = tmp->next;
+tmp->next = new;
+return (new);
 }
