97ae2ed171aded9989325b35d50ec0c155a48369
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-18 20:35:27 +0100

    3-set_bit.c

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit
+ * @n: number
+ * @index: index
+ *
+ * Return: return
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= (1 << index);
+
+	return (1);
+}
