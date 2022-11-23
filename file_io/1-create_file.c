4c53a39c66e63177c57a1d29ae88239c3d28db32
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-23 09:47:21 +0100

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@ Return: return
 */
  int create_file(const char *filename, char *text_content)
  {
-    int o, w, length = ;
+    int o, w, length = 0;
     if (filename == NULL)
         return (-1);
         if (text_content != NULL)
