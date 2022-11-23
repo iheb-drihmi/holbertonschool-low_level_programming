3eb665101227a68fb5d0b443f7d34629973197c9
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-23 09:45:09 +0100

    1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* create_file - a function that create a file
+* @filename: a pointer
+*@text_content: a pointer
+Return: return
+*/
+ int create_file(const char *filename, char *text_content)
+ {
+    int o, w, length = ;
+    if (filename == NULL)
+        return (-1);
+        if (text_content != NULL)
+        {
+            for (length = 0; text_content[length];)
+                length++;
+        }
+    o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    w = write(o, text_content, length);
+
+    if (o == -1 || w == -1)
+    return (-1);
+    close(o);
+    return (1);    
+ }
+ 
