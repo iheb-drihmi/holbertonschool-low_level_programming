3b0d983211931a4a165a14e302f1860c5b12e0b9
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-23 08:48:49 +0100

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include"main.h"
 #include<stdlib.h>
 /**
-*read_textfile - a function that reads a text file and prints it to the POSIX standard output
+*read_textfile - a function that reads a text file and prints
 @filename: a pointer
 @letters: the number of letter
 Return:return
@@ -11,19 +11,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 ssize_t o, r, w;
 char *b;
 if (filename == NULL)
-return(0);
+return (0);
 b = malloc(sizeof(char) * letters);
 if (b == NULL)
 return (0);
 o = open(filename, O_RDONLY);
 r = read(o, b, letters);
 w = write(STDOUT_FILENO, b, r);
-if (o == -1 || r == -1 || w == -1 || w !=r)
+if (o == -1 || r == -1 || w == -1 || w != r)
 {
 free(b);
-return(0);
+return (0);
 }
 free(b);
 close(o);
-return(w);    
+return (w);
 }
+
