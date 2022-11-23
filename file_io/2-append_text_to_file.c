7acdd8e128a86b58e27c18255fe847c7d3ab9735
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-23 10:22:31 +0100

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,7 +10,7 @@ int append_text_to_file(const char *filename, char *text_content)
     int o, w, length = 0;
     if (filename == NULL)
     return(-1);
-    else if (tesxt_content != NULL)
+    else if (text_content != NULL)
     {
         for (length = 0; text_content[length];)
         length++;
