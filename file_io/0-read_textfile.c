426b144cfd57bf999924aa7d076bc72e724b59e1
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-23 08:43:48 +0100

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+*read_textfile - a function that reads a text file and prints it to the POSIX standard output
+@filename: a pointer
+@letters: the number of letter
+Return:return
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    ssize_t o, r, w;
+    char *b;
+    if (filename == NULL)
+        return(0);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (b == NULL)
+    return (0);
+    o = open(filename, O_RDONLY);
+    r = read(o, b, letters);
+    w = write(STDOUT_FILENO, b, r);
+    if (o == -1 || r == -1 || w == -1 || w !=r)
+    {
+        free(b);
+        return(0);
+    }
+    free(b);
+    close(o);
+    return(w);    
+}
