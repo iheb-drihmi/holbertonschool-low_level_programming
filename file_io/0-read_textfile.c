bc92932acf7c282600e5ed5f9854aeb414c2d8be
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-23 08:45:50 +0100

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,23 +8,22 @@ Return:return
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t o, r, w;
-    char *b;
-    if (filename == NULL)
-        return(0);
-
-    b = malloc(sizeof(char) * letters);
-    if (b == NULL)
-    return (0);
-    o = open(filename, O_RDONLY);
-    r = read(o, b, letters);
-    w = write(STDOUT_FILENO, b, r);
-    if (o == -1 || r == -1 || w == -1 || w !=r)
-    {
-        free(b);
-        return(0);
-    }
-    free(b);
-    close(o);
-    return(w);    
+ssize_t o, r, w;
+char *b;
+if (filename == NULL)
+return(0);
+b = malloc(sizeof(char) * letters);
+if (b == NULL)
+return (0);
+o = open(filename, O_RDONLY);
+r = read(o, b, letters);
+w = write(STDOUT_FILENO, b, r);
+if (o == -1 || r == -1 || w == -1 || w !=r)
+{
+free(b);
+return(0);
+}
+free(b);
+close(o);
+return(w);    
 }
