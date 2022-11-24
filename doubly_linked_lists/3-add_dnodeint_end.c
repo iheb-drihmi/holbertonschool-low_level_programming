2eddd2acd689d2be579c2df69ef1f0c0e8504b0c
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-24 11:57:49 +0100

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,7 +9,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
     dlistint_t *new_node, *tmp;
-    new_node = malloc(sizeof(dlistint-t));
+    new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL || head == NULL)
     return (NULL);
     new_node->n = n;
