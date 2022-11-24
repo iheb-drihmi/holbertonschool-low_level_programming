9381b5f4a54a4093381b04479b7e4584440400de
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-24 11:58:40 +0100

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
     if (*head == NULL)
     {
         *head = new_node;
-        return = (new_node);
+        return (new_node);
 
     }
     else 
