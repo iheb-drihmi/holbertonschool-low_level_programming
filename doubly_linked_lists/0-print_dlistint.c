eb25914701c652ad7a5b9c1aa4652ddfbc975449
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-11-24 10:43:59 +0100

    doubly linked list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,20 +2,19 @@
 #include<stdlib.h>
 #include<stdio.h>
 /**
-* print_dlistint - a function that prints all the elements 
+* print_dlistint - a function that prints all the elements
 *@h: the header
 *Return: return
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    
-    size_t node = 0;
-    while (h)
-    {
-        node++;
-        printf("%d\n" , h->n);
-        h = h->next;
 
-    }
-    return (node);
+size_t node = 0;
+while (h)
+{
+node++;
+printf("%d\n" , h->n);
+h = h->next;
+}
+return (node);
 }
