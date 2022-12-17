74ea18026ca7f33aa9b2a1009174fd8c3b4284d9
iheb <iheb@DESKTOP-TQ7ID0S.localdomain>
2022-12-17 11:45:10 +0100

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * *leet - a function that encodes a string into 1337
+ * @str: string
+ * Return: return
+ */
+char *leet(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		if (str[index] == 'a' || str[index] == 'A')
+		{
+			str[index] = '4';
+		}
+		else if (str[index] == 'e' || str[index] == 'E')
+		{
+			str[index] = '3';
+		}
+		else if (str[index] == 'o' || str[index] == 'O')
+		{
+			str[index] = '0';
+		}
+		else if (str[index] == 't' || str[index] == 'T')
+		{
+			str[index] = '7';
+		}
+		else if (str[index] == 'l' || str[index] == 'L')
+		{
+			str[index] = '1';
+		}
+	}
+	return (str);
+}
+gi
