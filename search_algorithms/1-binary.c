1696df97cdc355ee687620b33116719cf916ea0d
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-04 21:04:35 +0100

    1-binary.c

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 /**
- * binary_search - a function that searches for a value in a sorted array of integer
+ * binary_search - a function that searches for a value in a sorted array
  * @array: array of integers
  * @size: size of array
  * @value: value to search
@@ -8,7 +8,7 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t j, left, right;
+size_t j, left, right;
 
 	if (array == NULL)
 		return (-1);
